af7dad18630ec2e9209dc9d57d0b6ccef8b8e46a
Katekani Madingana <madinganakn@gmail.com>
2023-12-05 11:16:07 +0200

    ev car 100

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,29 +1,17 @@
-// 10-car.js
-
-export default class Car {
-  constructor(brand, motor, color) {
-    this._brand = brand;
-    this._motor = motor;
-    this._color = color;
-  }
-
-  cloneCar() {
-    const constructor = Object.getPrototypeOf(this).constructor;
-    return new constructor(this._brand, this._motor, this._color);
-  }
-}
-
-// 100-evcar.js
-
-import Car from "./10-car.js";
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable class-methods-use-this */
+import Car from './10-car';
 
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
-    super(brand, motor, color);
+    super();
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
     this._range = range;
   }
 
   cloneCar() {
-    return super.cloneCar();
+    return new Car();
   }
 }
