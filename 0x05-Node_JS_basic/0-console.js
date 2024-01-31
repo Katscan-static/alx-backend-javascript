f505c7c49c27a858c4b5be930831aec4a111bc16
Katekani Madingana <madinganakn@gmail.com>
2024-01-31 11:25:04 +0200

    comments test

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,6 @@
-f// In the file 0-console.js, create a function named
-// displayMessage that prints in STDOUT the string argument function displayMessage(message) {
+// In the file 0-console.js, create a function named
+// displayMessage that prints in STDOUT the string argument 
+function displayMessage(message) {
   console.log(message);
 }
 
