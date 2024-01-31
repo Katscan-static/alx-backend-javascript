6f8f5d3bbb53509c90034a3b188a051b4fc68a15
Katekani Madingana <madinganakn@gmail.com>
2024-01-31 11:31:07 +0200

    1 stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,15 +1,10 @@
-process.stdin.setEncoding('utf8')
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-console.log("Welcome to Holberton School, what is your name?")
+process.stdin.on('readable', () => {
+  const input = process.stdin.read();
+  if (input !== null) process.stdout.write(`Your name is: ${input}`);
+});
 
-process.stdin.on('data', function(data) {
-	const input = data.trim();
-
-	if (input !== '') {
-		console.log("Your name is: " + input);
-	}
-	else {
-		console.log('This important software is now closing')
-		process.exit()
-	}
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
