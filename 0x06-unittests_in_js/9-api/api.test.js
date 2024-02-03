1ff05f36956f5cdb60c59311c8e8e4ae47922f25
Katekani Madingana <madinganakn@gmail.com>
2024-02-03 11:05:06 -0800

    9 api

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,37 @@
+const request = require('request');
+const expect = require('chai').expect;
+
+describe('Index page', () => {
+  it('should return a 200 status code', (done) => {
+    request('http://localhost:7865', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('should return the correct message', (done) => {
+    request('http://localhost:7865', (error, response, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
+  describe('Cart page', () => {
+    it('should return a 200 status code when :id is a number', (done) => {
+      request('http://localhost:7865/cart/12', (error, response, body) => {
+        expect(response.statusCode).to.equal(200);
+        expect(body).to.equal('Payment methods for cart 12');
+        done();
+      });
+    });
+  
+    it('should return a 404 status code when :id is not a number', (done) => {
+      request('http://localhost:7865/cart/hello', (error, response, body) => {
+        expect(response.statusCode).to.equal(404);
+        done();
+      });
+    });
+  
+    
+  });
+});
